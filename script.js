8e0e7824c8ccc794207804dcae16366e52eae107
Adakole Naomie <adakolenaomi049@gmail.com>
2025-01-17 02:49:00 -0800

    c

--- script.js
+++ script.js
@@ -35,3 +35,41 @@ const observer1 = new IntersectionObserver(entries => {
 box24.forEach(box24 =>{
     observer.observe(box24)
 })
+
+const navone = document.querySelectorAll(".navone")
+
+const observer2 = new IntersectionObserver(entries => {
+    
+    entries.forEach(entry =>{
+        entry.target.classList.toggle("show", entry.isIntersecting)
+
+        // the berlow commented code is for if you don't want the effect to be repetitive
+        // if (entry.isIntersecting) observer.unobserve(entry.target)
+        console.log(entries)
+    })
+}, {
+    // threshold: 1,
+    rootMargin:"-100px",
+})
+navone.forEach(navone =>{
+    observer.observe(navone)
+})
+
+const box1 = document.querySelectorAll(".box1")
+
+const observer3 = new IntersectionObserver(entries => {
+    
+    entries.forEach(entry =>{
+        entry.target.classList.toggle("show", entry.isIntersecting)
+
+        // the berlow commented code is for if you don't want the effect to be repetitive
+        // if (entry.isIntersecting) observer.unobserve(entry.target)
+        console.log(entries)
+    })
+}, {
+    // threshold: 1,
+    rootMargin:"-100px",
+})
+box1.forEach(box1 =>{
+    observer.observe(box1)
+})
